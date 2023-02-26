d5219a41280a45b01ef821b4d1193fd570f05659
Semora <senalweerasekara@gmail.com>
2023-02-26 14:36:29 +0530

    starting over

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,19 +3,10 @@ import styled from 'styled-components'
 import Header from '../../components/Header'
 import Banner from './Banner'
 
-const HeaderContainer = styled.div`
-  position: fixed;
-  width: 100%;
-`
-
 export default function Home() {
   return (
     <>
-      <HeaderContainer>
-        <Header type="home" />
-      </HeaderContainer>
-
-      <Banner />
+      <Header type="home" />
     </>
   )
 }
