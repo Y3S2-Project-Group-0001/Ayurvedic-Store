171430f7b5f7d6db44dca44a7061da12671508a2
Semora <senalweerasekara@gmail.com>
2023-02-27 15:11:42 +0530

    Header is mostly done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,125 +1,148 @@
 import React from 'react'
 import styled from 'styled-components'
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 
-let textColor = 'white'
-let bgColor = 'black'
+const MainContainer = styled.div`
+  position: fixed;
+  top: 0%;
+  width: 100%;
+`
 
 const Container = styled.div`
   display: flex;
   align-items: center;
+  justify-content: space-between;
   width: 90%;
   margin: 0 auto;
   padding: 1rem 0;
 `
 
-export default function Header(props) {
-  const [login, setLogin] = useState('notLogged')
+const Nav = styled.div`
+  span {
+    margin-left: 2rem;
+    a {
+      color: ${props => props.color};
+      transition: 0.5s;
+      text-decoration: none;
+      font-weight: 400;
+      font-family: 'Quicksand';
+    }
+  }
+`
+const Logo = styled.div`
+  font-weight: 400;
+  font-family: 'Quicksand';
+  font-size: 30px;
+`
 
-  if (props.type == 'home') {
-    textColor = 'black'
-    bgColor = 'rgba(0, 0, 0, 0)'
-  } else {
-    bgColor = 'rgba(61, 86, 49, 1)'
-    textColor = 'white'
+const Login = styled.div`
+  align-items: center;
+  display: flex;
+  div {
+    margin-left: 1rem;
+    a {
+      color: ${props => props.color};
+      transition: 0.5s;
+      text-decoration: none;
+      font-weight: 400;
+      font-family: 'Quicksand';
+    }
+  }
+`
+
+const OuterContainer = styled.div`
+  background-color: ${props => props.background};
+  color: ${props => props.color};
+  transition: 0.5s;
+`
+
+const ButtonDash = styled.div`
+  margin-right: 45px;
+  font-size: 15px;
+  font-weight: 400;
+  font-family: 'Quicksand';
+  border-color: white;
+  border-style: solid;
+  padding: 10px;
+  padding-left: 20px;
+  padding-right: 20px;
+`
+
+export default function Header(props) {
+  const [textColor, setTextColor] = useState('white')
+  const [bgColor, setBgColor] = useState('rgba(61, 86, 49, 1)')
+  const [login, setLogin] = useState('notLogged')
+  const [scrolled, setScrolled] = useState(true)
+
+  const changeBackground = () => {
+    if (window.scrollY > 100) {
+      setScrolled(false)
+    } else {
+      setScrolled(true)
+    }
   }
 
-  const Nav = styled.div`
-    margin-left: 20%;
-    span {
-      margin-left: 2rem;
-      a {
-        color: ${textColor};
-        text-decoration: none;
-        font-weight: 400;
-        font-family: 'Quicksand';
-      }
+  window.addEventListener('scroll', changeBackground)
+
+  useEffect(() => {
+    if (props.type == 'home' && scrolled) {
+      setTextColor('black')
+      setBgColor('rgba(0, 0, 0, 0.1)')
+    } else {
+      setTextColor('white')
+      setBgColor('rgba(61, 86, 49, 1)')
     }
-  `
-
-  const Logo = styled.div`
-    font-weight: 400;
-    font-family: 'Quicksand';
-    font-size: 30px;
-  `
-
-  const Login = styled.div`
-    /* max-width: 200px; */
-    margin-left: auto;
-    div {
-      margin-left: 1rem;
-      a {
-        color: ${textColor};
-        text-decoration: none;
-        font-weight: 400;
-        font-family: 'Quicksand';
-      }
-    }
-  `
-
-  const OuterContainer = styled.div`
-    background-color: ${bgColor};
-    color: ${textColor};
-  `
-
-  const ButtonDash = styled.div`
-    margin-left: 20%;
-    font-size: 15px;
-    font-weight: 400;
-    font-family: 'Quicksand';
-    border-color: white;
-    border-style: solid;
-    padding: 10px;
-    padding-left: 20px;
-    padding-right: 20px;
-  `
+  }, [props, scrolled])
 
   return (
     <>
-      <link
-        href="https://fonts.googleapis.com/css?family=Quicksand"
-        rel="stylesheet"
-      ></link>
-      <OuterContainer>
-        <Container>
-          <Logo>CEYLONHERB</Logo>
+      <MainContainer>
+        <link
+          href="https://fonts.googleapis.com/css?family=Quicksand"
+          rel="stylesheet"
+        ></link>
 
-          <Nav>
-            <span>
-              {' '}
-              <a href="#">Home</a>{' '}
-            </span>
-            <span>
-              {' '}
-              <a href="">Categories</a>{' '}
-            </span>
-            <span>
-              {' '}
-              <a href="">About us</a>{' '}
-            </span>
-            <span>
-              {' '}
-              <a href="">More</a>{' '}
-            </span>
-          </Nav>
+        <OuterContainer color={textColor} background={bgColor}>
+          <Container>
+            <Logo>CEYLONHERB</Logo>
 
-          <ButtonDash>Seller Dash</ButtonDash>
+            <Nav color={textColor}>
+              <span>
+                {' '}
+                <a href="#">Home</a>{' '}
+              </span>
+              <span>
+                {' '}
+                <a href="">Categories</a>{' '}
+              </span>
+              <span>
+                {' '}
+                <a href="">About us</a>{' '}
+              </span>
+              <span>
+                {' '}
+                <a href="">More</a>{' '}
+              </span>
+            </Nav>
 
-          <Login>
-            {login == 'Logged' ? (
-              <div>
-                <a href="">Logout </a>
-              </div>
-            ) : (
-              <div>
-                <a href="">Login</a>
-                <span> | </span>
-                <a href="">Signup</a>
-              </div>
-            )}
-          </Login>
-        </Container>
-      </OuterContainer>
+            <Login color={textColor}>
+              <ButtonDash>Seller Dash</ButtonDash>
+
+              {login == 'Logged' ? (
+                <div>
+                  <a href="">Logout </a>
+                </div>
+              ) : (
+                <div>
+                  <a href="">Login</a>
+                  <span> | </span>
+                  <a href="">Signup</a>
+                </div>
+              )}
+            </Login>
+          </Container>
+        </OuterContainer>
+      </MainContainer>
     </>
   )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,10 +3,41 @@ import styled from 'styled-components'
 import Header from '../../components/Header'
 import Banner from './Banner'
 
+const Test = styled.div`
+  position: fixed;
+`
+
 export default function Home() {
   return (
     <>
+      <Test></Test>
       <Header type="home" />
+
+      <h1>ddd</h1>
+      <h1>ddd</h1>
+      <h1>ddd</h1>
+      <h1>ddd</h1>
+
+      <h1>ddd</h1>
+      <h1>ddd</h1>
+      <h1>ddd</h1>
+      <h1>ddd</h1>
+      <h1>ddd</h1>
+      <h1>ddd</h1>
+      <h1>ddd</h1>
+      <h1>ddd</h1>
+      <h1>ddd</h1>
+      <h1>ddd</h1>
+      <h1>ddd</h1>
+      <h1>ddd</h1>
+      <h1>ddd</h1>
+      <h1>ddd</h1>
+      <h1>ddd</h1>
+      <h1>ddd</h1>
+      <h1>ddd</h1>
+      <h1>ddd</h1>
+      <h1>ddd</h1>
+      <h1>ddd</h1>
     </>
   )
 }
