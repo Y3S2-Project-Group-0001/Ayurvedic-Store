a6285010d5d864a78d7cd929f084cbbbfbf837ae
Semora <senalweerasekara@gmail.com>
2023-03-01 09:22:10 +0530

    Footer added

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -24,6 +24,9 @@ const HeroContent = styled.div`
   position: absolute;
   top: 50%;
   left: 25%;
+  background-color: #7590680;
+  padding: 50px;
+  border-radius: 20px;
   @media only screen and (max-width: 1000px) {
     left: 50%;
     background-color: #759068bd;
