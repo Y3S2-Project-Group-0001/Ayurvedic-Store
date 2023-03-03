5154ce0008e884fb59089a8be4a41b3c87d27ea6
KaushalyaPremarathne <it20633622@my.sliit.lk>
2023-03-03 12:31:20 +0000

    add seller routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
-import CustomerRouter from "./routes/customer";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import reportWebVitals from './reportWebVitals'
+import { BrowserRouter } from 'react-router-dom'
+import CustomerRouter from './routes/customer'
+import SellerRouter from './routes/seller'
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <CustomerRouter />
+      <SellerRouter />
     </BrowserRouter>
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
