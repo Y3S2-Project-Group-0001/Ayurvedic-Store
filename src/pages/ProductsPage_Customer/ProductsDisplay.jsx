5c7ffd1a6ea63f718bac436cd4b684b417d39d2e
KaushalyaPremarathne <it20633622@my.sliit.lk>
2023-03-06 07:07:57 +0000

    Basic UI of Products View

---
+++ src/pages/ProductsPage_Customer/ProductsDisplay.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import styled from 'styled-components'
+import ProductCard from './ProductCard'
+
+const Container = styled.div`
+  height: 100%;
+  display: flex;
+  justify-content: center;
+`
+
+const GridContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr); /* 4 columns */
+  grid-gap: 20px; /* spacing between grid items */
+`
+
+function ProductsDisplay() {
+  return (
+    <Container>
+      <GridContainer>
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+      </GridContainer>
+    </Container>
+  )
+}
+
+export default ProductsDisplay
