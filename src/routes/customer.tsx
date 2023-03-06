6f06b9389375a78848ae9ca42e96c8cc665d4a98
KaushalyaPremarathne <it20633622@my.sliit.lk>
2023-03-06 07:11:53 +0000

    add routes of customer products page

--- src/routes/customer.tsx
+++ src/routes/customer.tsx
@@ -4,6 +4,7 @@ import { FileUpload } from '../pages/examples/file-upload'
 import Home from '../pages/Home/Home'
 import ShoppingCart from '../pages/ShoppingCart'
 import PaymentSelect from '../pages/PaymentSelect/PaymentSelect'
+import ProductsDisplay from '../pages/ProductsPage_Customer/ProductsDisplay'
 
 export default function CustomerRouter() {
   return (
@@ -13,6 +14,8 @@ export default function CustomerRouter() {
       <Route path="/shoppingCart" element={<ShoppingCart />} />
 
       <Route path="/TestingPage" element={<PaymentSelect />} />
+
+      <Route path="/allProductsCustomer" element={<ProductsDisplay />} />
     </Routes>
   )
 }
