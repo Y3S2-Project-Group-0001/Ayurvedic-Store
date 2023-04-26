5cc7c10c8dceb3a5735a4a4c1032935701c3546f
KaushalyaPremarathne <it20633622@my.sliit.lk>
2023-04-26 08:04:32 +0100

    single product view product part completed

--- src/routes/seller.tsx
+++ src/routes/seller.tsx
@@ -5,26 +5,23 @@ import AddProduct from '../pages/AddProducts/AddProduct'
 import ProductsPageSeller from '../pages/ProductsPage_Seller/ProductsPageSeller'
 import UpdateProductForm from '../pages/UpdateProduct/UpdateProductForm'
 
-import ProductsPage from '../pages/ProductsPage_Seller/ProductsPage'
 import UpdateProduct from '../pages/UpdateProduct/UpdateProduct'
 import MainLayout from '../pages/MainLayout'
 import DashboardLayout from '../pages/DashboardLayout'
 import Orders from '../pages/Orders'
 import ViewOrderAdmin from '../pages/VIewOrderAdmin/ViewOrderAdmin'
 
-
 export default function SellerRouter() {
   return (
     <Routes>
       <Route path="/addProduct" element={<AddProduct />} />
-
 
       <Route path="/allProductSeller" element={<ProductsPageSeller />} />
 
       <Route path="/updateProduct/:id" element={<UpdateProductForm />} />
 
       <Route path="/updateProduct" element={<UpdateProduct />} />
-      <Route path="/allProductSeller" element={<ProductsPage />} />
+      <Route path="/allProductSeller" element={<ProductsPageSeller />} />
       <Route path="/admin" element={<MainLayout />}>
         <Route path="/admin/dashBoard" element={<DashboardLayout />}>
           <Route path="/admin/dashBoard/orders" element={<Orders />}></Route>
@@ -34,7 +31,6 @@ export default function SellerRouter() {
           ></Route>
         </Route>
       </Route>
-
     </Routes>
   )
 }
